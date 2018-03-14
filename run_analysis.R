efe2cf5d5492067120233899e67855b7400e01d6
scgou <scghtc@gmail.com>
2018-03-14 14:42:39 +0000

    Update

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,7 @@
-X11
+# Create the directory for the data, download the zip file and unzip
+if (!file.exists("./data")) { dir.create("./data") }
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+destFile <- "./data/dataset.zip"
+download.file(fileURL, destfile = destFile, method = "curl")
+unzip(destFile, exdir = "./data")
+
